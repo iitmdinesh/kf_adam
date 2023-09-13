6443f61850e34f149099057b057a8751d69802cd
Dinesh Ramasamy <iitm.dinesh@gmail.com>
2023-09-13 05:31:38 -0700

    low memory footprint optimizer

--- kf_adam.py
+++ kf_adam.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 from torch.optim.optimizer import Optimizer
 
@@ -24,13 +26,16 @@ class KFAdam(Optimizer):
             params,
             lr: float = 1e-3,
             beta: float = 0.95,
-            step_size_limit: float = 1.0,
+            step_size_limit: Optional[float] = None,
             eps: float = 1e-6,
     ):
         if lr <= 0.0:
             raise ValueError('Invalid learning rate: {}'.format(lr))
-        if step_size_limit < 0.0:
-            raise ValueError('Invalid step_size_limit value: {}'.format(step_size_limit))
+        if step_size_limit is None:
+            step_size_limit = lr
+        else:
+            if step_size_limit < 0.0:
+                raise ValueError('Invalid step_size_limit value: {}'.format(step_size_limit))
         if not (0.0 < beta < 1.0):
             raise ValueError('Invalid beta value: {}'.format(beta))
         if eps < 0.0:
@@ -76,54 +81,53 @@ class KFAdam(Optimizer):
                     continue
 
                 grad = p.grad.data / (eps + norm)
+                where = grad != 0
                 if grad.is_sparse:
                     raise RuntimeError(
                         'KFAdam does not support sparse gradients, '
                         'please consider SparseAdam instead'
                     )
-                where = grad.abs() > 0
                 state = self.state[p]
                 if len(state) == 0:
-                    sigma_init = (grad.pow(2)[where].mean() + eps).sqrt()
-                    state['iter'] = where.long()
+                    sigma_sq_init = grad[where].pow(2).mean()
+                    state['iter'] = 1
                     state['estimate'] = torch.zeros_like(
                         p.data, memory_format=torch.preserve_format
                     )
-                    state['estimate_error'] = sigma_init * torch.ones_like(
+                    state['obs_prev'] = torch.zeros_like(
                         p.data, memory_format=torch.preserve_format
                     )
-                    state['process_variance'] = sigma_init * torch.ones_like(
+                    state['obs_one_before_prev'] = torch.zeros_like(
                         p.data, memory_format=torch.preserve_format
                     )
-                    state['measurement_variance'] = sigma_init * torch.ones_like(
-                        p.data, memory_format=torch.preserve_format
-                    )
+                    state['estimate_error'] = sigma_sq_init
+                    state['obs_prev_var'] = sigma_sq_init
+                    state['obs_one_before_prev_var'] = sigma_sq_init
+                state['obs_prev_var'] = \
+                    state['obs_prev_var'] * beta + (1 - beta) * (grad - state['obs_prev'])[where].pow(2).mean()
+                state['obs_one_before_prev_var'] = state['obs_one_before_prev_var'] * beta + \
+                    (1 - beta) * (grad - state['obs_one_before_prev'])[where].pow(2).mean()
+                # Reference: http://article.nadiapub.com/IJCA/vol10_no10/6.pdf
+                measurement_variance = ((state['obs_prev_var'] - 0.5 * state['obs_one_before_prev_var'])
+                                        / (1.0 - beta ** state['iter'])).clamp(min=eps)
+                process_variance = ((state['obs_one_before_prev_var'] - state['obs_prev_var'])
+                                        / (1.0 - beta ** state['iter'])).clamp(min=eps)
                 prediction = state['estimate']
-                measurement_variance = state['measurement_variance']
-                process_variance = state['process_variance']
                 prediction_error = state['estimate_error'] + process_variance
-                kalman_gain = prediction_error / (eps + prediction_error + measurement_variance)
+                kalman_gain = (prediction_error / (eps + prediction_error + measurement_variance)).clamp(min=0.0, max=1.0)
                 innovation = grad - prediction
                 estimate = prediction + kalman_gain * innovation
-                estimate_error = (1.0 - kalman_gain).clamp(min=0.0) * prediction_error
-                state['estimate_error'] = torch.where(where, estimate_error, state['estimate_error'])
+                estimate_error = (1.0 - kalman_gain) * prediction_error
+                state['estimate_error'] = estimate_error
                 state['estimate'] = torch.where(where, estimate, state['estimate'])
-                state['process_variance'] = torch.where(
-                    where, process_variance * beta + (1.0 - beta) * innovation.pow(2), state['process_variance'])
-                state['measurement_variance'] = torch.where(
-                    where,
-                    measurement_variance * beta + (1.0 - beta) * (grad - estimate).pow(2),
-                    state['measurement_variance']
-                )
 
-                step = -lr * norm * estimate / \
-                       (torch.sqrt(estimate_error /
-                                   (1.0 - torch.where(where, beta ** state['iter'], torch.zeros_like(estimate_error))))
-                        + eps)
+                step = -lr * norm * estimate / (torch.sqrt(estimate_error) + eps)
                 step = step.clamp(-step_size_limit, step_size_limit)
                 step = torch.where(where, step, torch.zeros_like(step))
 
                 p.data.add_(step)
-                state['iter'] = state['iter'] + where.long()
+                state['iter'] += 1
+                state['obs_one_before_prev'] = torch.where(where, state['obs_prev'], state['obs_one_before_prev'])
+                state['obs_prev'] = torch.where(where, grad, state['obs_prev'])
 
         return loss
